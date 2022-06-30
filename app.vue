284e17700f7ee220f38a364f4cbd4e6a5fa52667
kaiiy <kaiiy@kaiix.tokyo>
2022-07-01 01:41:03 +0900

    feat: add banner

--- app.vue
+++ app.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <div>白一面</div>
-  <div>制作: かい</div>
-  <div>注意事項: 白には触れない</div>
+  <div class="relative">
+
+    <div class="relative flex justify-center items-center">
+      <div class="w-full h-full bg-gray-900 bg-opacity-80 top-0 fixed sticky-0 px-2 py-2">
+
+        <!-- todo: py-[] -->
+        <div class="2xl:container 2xl:mx-auto flex justify-center items-center">
+          <div
+            class="w-96 md:w-auto relative flex flex-col justify-center items-center bg-white py-20 md:py-14 lg:py-16 xl:py-20 px-4 md:px-24 xl:px-30">
+            <div>
+              <img class="w-20 lg:w-28 select-none" src="@/assets/img/banner.webp" alt="">
+            </div>
+            <div class="mt-12 md:mt-8 lg:mt-12">
+              <h1
+                class="text-3xl lg:text-4xl font-semibold leading-7 lg:leading-9 text-center text-gray-800 select-none">
+                白一面</h1>
+            </div>
+            <div class="mt-6 md:mt-4">
+              <p class="sm:w-80 text-base  leading-7 text-center text-gray-800 select-none">制作: かい</p>
+              <p class="sm:w-80 text-base  leading-7 text-center text-gray-800 select-none">注意事項: 白には触れない</p>
+            </div>
+            <button
+              class="w-full sm:w-auto mt-12 md:mt-8 lg:mt-12 text-base leading-4 text-center text-white py-3 md:py-4 md:py-4 px-20 md:px-24 bg-gray-800 hover:bg-gray-900 select-none">スタート</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
